cf45d1ff5e52d5a40d138ec9112b8fae9e04042b
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-12-20 11:21:09 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 
@@ -9,11 +9,11 @@ import {HashRouter} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <HashRouter>
+     <BrowserRouter>
         <Header/>
         <Main/>
         <Footer/>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
